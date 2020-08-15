566a51c266cd73038bd1a6da093cd75c228bfdf1
Ankit Khatiwada <akgunner6@gmail.com>
2020-08-15 22:59:43 +0545

    Points and history

--- client/src/History.js
+++ client/src/History.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function History(props) {
     return (
-        <div>
+        <div className="history">
 
         </div>
     );
--- client/src/Points.js
+++ client/src/Points.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { useStateValue } from './StateProvider';
 
 function Points(props) {
-    const [{ basket, isAdmin }] = useStateValue();
-    console.log(isAdmin)
     return (
         <div className="points">
 
