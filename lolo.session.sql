d617b5b4962f22658b6564814b4967b055d203e8
Nischal <nbhn333@gmail.com>
2020-09-11 13:19:59 +0545

    make it better

--- lolo.session.sql
+++ lolo.session.sql
@@ -1,10 +1,1 @@
-CREATE TABLE basket(
-   userName VARCHAR(255) ,
-   modelNo VARCHAR(255),
-   quantity INT,
-   eventId INT ,
-   PRIMARY KEY (userName,modelNo),
-   FOREIGN KEY (userName) REFERENCES user(userName),
-   FOREIGN KEY (modelNo) REFERENCES giftShop(modelNo),
-   FOREIGN KEY (eventId) REFERENCES event(id)
- );
+
